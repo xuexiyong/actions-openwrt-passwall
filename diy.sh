561b57e3c901a2963fef1219abb81267aed3720a
xuexiyong <19756594+xuexiyong@users.noreply.github.com>
2020-05-16 10:54:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
